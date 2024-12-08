bcd8172ceefd8dcdc6674b5f4a55d090c6b4eb39
David Meadows <dtmeadows@stainlessapi.com>
2024-12-08 16:31:03 -0500

    better testing on nearby

--- tests/api/test_get_nearby_observations.py
+++ tests/api/test_get_nearby_observations.py
@@ -1,6 +1,6 @@
 import pytest
 from cloaca.api.get_nearby_observations import get_nearby_observations
-from cloaca.parsing.parsing_helpers import Lifer
+from cloaca.parsing.parsing_helpers import Lifer, Location
 from cloaca.types import set_lifers_to_cache
 
 DEFAULT_LONGITUDE = -74.0242
@@ -9,7 +9,7 @@ DEFAULT_LATITUDE = 40.6941
 
 @pytest.mark.asyncio
 @pytest.mark.vcr
-async def test_regional_lifers():
+async def test_get_nearby_observations():
     set_lifers_to_cache(
         "key",
         [
@@ -31,4 +31,24 @@ async def test_regional_lifers():
         DEFAULT_LATITUDE, DEFAULT_LONGITUDE, "key"
     )
 
-    assert len(nearby_observations) == 1429
+    assert len(nearby_observations) > 1
+
+    # northern harrier code should be filtered out
+    for obs in nearby_observations.values():
+        # map through obs.lifers to find the one with the same species code
+        lifer = next(
+            (lifer for lifer in obs.lifers if lifer.species_code == "norhar2"),
+            None,
+        )
+
+        assert lifer is None
+
+    central_park = nearby_observations["L191106"]
+    expected = Location(
+        location_name="Central Park",
+        latitude=40.7715482,
+        longitude=-73.9724819,
+        location_id="L191106",
+    )
+    assert central_park.location == expected
+    assert len(central_park.lifers) > 1
