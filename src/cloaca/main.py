75c31b98fa39d0e54ee942015d0481e5b27812e9
David Meadows <dtmeadows@stainlessapi.com>
2024-12-08 16:16:59 -0500

    fixup!

--- src/cloaca/main.py
+++ src/cloaca/main.py
@@ -1,23 +1,14 @@
-import asyncio
 import time
-from typing import List
 from uuid import uuid4
 
 from cloaca.api.get_new_lifers_by_region import (
     get_filtered_lifers_for_region,
 )
-from cloaca.api.shared import round_to_nearest_half
 from cloaca.parsing.parse_ebird_personal_export import parse_csv_from_file_to_lifers
 from cloaca.parsing.parsing_helpers import Lifer
-from cloaca.api.shared import (
-    fetch_nearby_observations_from_ebird_with_cache,
-    fetch_nearby_observations_of_species_from_ebird_with_cache,
-)
 from cloaca.types import (
-    filter_lifers_from_nearby_observations,
     get_lifers_from_cache,
     group_lifers_by_location,
-    phoebe_observation_to_lifer,
     set_lifers_to_cache,
 )
 
@@ -66,52 +57,7 @@ def health_check():
 
 @Cloaca_App.get("/v1/nearby_observations")
 async def get_nearby_observations(latitude: float, longitude: float, file_id: str):
-    request_start_time = time.time()
-    nearby_observations = await fetch_nearby_observations_from_ebird_with_cache(
-        round_to_nearest_half(latitude), round_to_nearest_half(longitude)
-    )
-
-    lifers_from_csv = get_lifers_from_cache(file_id)
-
-    unseen_species = filter_lifers_from_nearby_observations(
-        nearby_observations, lifers_from_csv
-    )
-
-    unseen_species_codes = list(
-        set([species.species_code for species in unseen_species])
-    )
-
-    print(f"unseen species codes: {unseen_species_codes}")
-
-    # use async.gather to make multiple requests at once
-    unseen_lifers: List[Lifer] = []
-
-    async def fetch_task(species_code):
-        unseen_observations_for_species = await (
-            fetch_nearby_observations_of_species_from_ebird_with_cache(
-                species_code,
-                round_to_nearest_half(latitude),
-                round_to_nearest_half(longitude),
-            )
-        )
-
-        for observation in unseen_observations_for_species:
-            unseen_lifers.append(phoebe_observation_to_lifer(observation))
-
-    fetch_tasks = [
-        fetch_task(unseen_species_code) for unseen_species_code in unseen_species_codes
-    ]
-
-    await asyncio.gather(*fetch_tasks)
-
-    lifers_by_location = group_lifers_by_location(unseen_lifers)
-
-    print("returning", len(lifers_by_location), "locations")
-
-    duration = time.time() - request_start_time
-    print(f"request took {duration} seconds")
-
-    return lifers_by_location
+    return get_nearby_observations(latitude, longitude, file_id)
 
 
 @Cloaca_App.get("/v1/lifers_by_location")
