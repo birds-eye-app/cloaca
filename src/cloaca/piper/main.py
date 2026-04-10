dd6b274f8edbfa2b934657961894543971dd1183
dtmeadows <davidtmeadows@gmail.com>
2026-04-10 18:05:20 -0400

    Change BirdCast migration alert from 10 PM UTC to 7 AM Eastern (#18)

--- src/cloaca/piper/main.py
+++ src/cloaca/piper/main.py
@@ -162,7 +162,7 @@ async def check_year_lifers():
             logger.info("no new lifers at %s", hotspot.name)
 
 
-@tasks.loop(time=datetime.time(hour=22, minute=0, tzinfo=datetime.timezone.utc))
+@tasks.loop(time=datetime.time(hour=7, minute=0, tzinfo=_EASTERN))
 async def post_birdcast_forecast():
     channel = bot.get_channel(BIRDCAST_CHANNEL_ID)
     if channel is None:
